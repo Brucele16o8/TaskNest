0ef6e49726231fd4f2f7e34cdf45035a12e9e8e5
Bruce Le <brucele.tt168@gmail.com>
2025-01-22 18:39:19 +1100

    Initial upload for Front-end

---
+++ TaskNest/TaskNest/TaskNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskNestApp.swift
+//  TaskNest
+//
+//  Created by Tung Le on 22/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
